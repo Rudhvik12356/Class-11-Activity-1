0a295ac06c6ffb3589e2097a0f1111027b26610c
Rudhvik12356 <123496122+Rudhvik12356@users.noreply.github.com>
2023-01-31 17:20:52 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+for(var i=0;i<weight.length;i++){
+//console.log(weight[i])
+if(weight[i]>40){
+  console.log(weight[i])
+}
+}
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
